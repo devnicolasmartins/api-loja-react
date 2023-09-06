b13625d6ef522e0d5931baaba3bde62610be82cb
devnicolasmartins <nicolasmartins803@gmail.com>
2023-09-05 23:43:33 -0300

    Correção

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,7 @@ import fetchProducts from "../../services/Api.js";
 
 const CardList = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState(null);
+    const [cart, setCart] = useState(0);
 
     useEffect(() => {
         const getProducts = async () => {
